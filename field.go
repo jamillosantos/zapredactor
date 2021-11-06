3019eb425eadc1c25728e9b1f3d9c65bc2fb08b2
Jota Santos <jamillo@gmail.com>
2021-11-06 03:17:03 -0300

    fix: Fix bug for nested structs + docs (#2)

--- field.go
+++ field.go
@@ -17,6 +17,11 @@ func (p *redactableProxy) MarshalLogObject(encoder zapcore.ObjectEncoder) error 
 	return p.Redact(encoder)
 }
 
+// Redact adds a field to a zap log entry which its content is redacted.
+// If the object implements the Redactable, its Redact method will be used to add the fields;
+// If the object implements a zapcore.ObjectMarshaler it will be used to log the structure;
+// Otherwise, the TagRedactor will wrap the given value redacting all fields by default, unless the ones explicitly
+// marked as allowed.
 func Redact(key string, val interface{}) zap.Field {
 	switch v := val.(type) {
 	case Redactable:
